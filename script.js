a253c1b5e0f7bd4dad141e32bf35c65c724387a8
nnayfin <nathan.pacheco.a@gmail.com>
2025-05-19 20:58:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// Declare and Use Variables
+var studentName = "Nathan";
+var studentAge = 23;
+var isEnrolled = true;
+
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+// Demonstrate Data Types
+// String
+let firstName = "John";
+let lastName = 'Doe';
+
+console.log(firstName);
+console.log(lastName);
+
+// Number
+let kilometers = 350.9;
+
+console.log(kilometers);
+
+// Boolean
+let dry = true;
+let wet = false;
+
+console.log(dry);
+console.log(wet);
+
+// Array
+let weather = ['Sun', 'Rain', 'Snow'];
+
+console.log(weather);
+
+// Object
+const movie = {
+    movieName: "Indiana Jones Raiders of the Lost Ark",
+    genreName: "Action",
+    rating: 100
+};
+
+console.log(movie);
+
+// Use Basic Operators
+// Arithmetic
+
+console.log(10 * 5); // Multiplication
+console.log(20 / 2); // Division
+console.log(59 + 10); // Addition
+console.log(430 - 10); // Subtraction
+
+// Comparison
+
+let x = 30;
+let y = 70;
+console.log(x < y); // Less than
+console.log(x > y); // Greater than
+console.log(x !== y); // Not equal
+console.log("70" === y); // Equal to
+
+// Logical
+
+let a = true;
+let b = false;
+console.log(a && b); // AND
+console.log(a || b); // OR
+console.log(!a); // NOT
